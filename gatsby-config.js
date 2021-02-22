c9a2656926cca6e1803e9b787a07708ff5548fac
sidhantmathur <sidhant185@gmail.com>
2021-02-22 14:17:48 -0600

    add config metadata and theme plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,32 @@
 module.exports = {
+    siteMetadata: {
+      title: `Sidhant Mathur's Portfolio`,
+      description: `Software projects and blog posts by Sidhant Mathur`,
+      author: `Sidhant Mathur`,
+      social: [
+        {
+          name: `Twitter`,
+          url: `https://twitter.com/gatsbyjs`,
+        },
+        {
+          name: `GitHub`,
+          url: `https://github.com/sidhantmathur`,
+        },
+      ],
+    },
     plugins: [
-
-    ]
-}
+      {
+        resolve: `gatsby-theme-blog`,
+        options: {
+          basePath: `/blog`,
+        },
+      },
+      {
+        resolve: `gatsby-theme-notes`,
+        options: {
+          basePath: `/notes`,
+        },
+      },
+    ],
+  }
+  
